b9fc71d65f0d05c8c0e02dc504351a79cde23911
SimoneBarone <simonebarone.5@gmail.com>
2023-11-15 12:18:32 +0100

    Creato array del mazzo di carte

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,39 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+	
+	Scanner sc=new Scanner(System.in);
+	String[]mazzo = {"1b","2b","3b","4b","5b","6b","7b","donnab","cavallob","reb",
+			"1d","2d","3d","4d","5d","6d","7d","donnad","cavallod","red",
+	        "1s","2s","3s","4s","5s","6s","7s","donnab","cavallos","res",
+	        "1c","2c","3c","4c","5c","6c","7c","donnac","cavalloc","rec"};
+	
+	
+	
+	
+			
+			
+	
+	
+	
+	
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
